6b61033044b9ab24495a6dd9ed8b5a11218d4f90
henry1479 <fokys.81@mail.ru>
2021-12-06 21:29:39 +0300

    lesson_php_7

---
+++ lesson_php_7/auth/index.php
@@ -0,0 +1,7 @@
+<form method="post" action="formHandler.php">
+    <p>Enter your login</p>
+    <input name="login" type="text">
+    <p>Enter your password</p>
+    <input name="password" type="password">
+    <input type="submit" value="send">
+</form>
